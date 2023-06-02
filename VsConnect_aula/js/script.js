de4963b28762572482e81f130baca5524b0c29ab
Nicolas Henrique <134559356+nclhenri@users.noreply.github.com>
2023-06-02 19:33:48 -0300

    Add files via upload

---
+++ VsConnect_aula/js/script.js
@@ -0,0 +1,15 @@
+let menu = document.getElementById("menu_links");
+let menu_barras = document.getElementById("menu_barras");
+
+function mostrarMenu (){
+    if (window.getComputedStyle(menu).display == "none") {
+        menu.style.display = "initial"
+        menu_barras.setAttribute("aria-label", "fechar menu")
+        menu_barras.setAttribute("aria-expanded", "true" )
+    }else{
+        menu.style.display = "none"
+        menu_barras.setAttribute("aria-label", "abrir menu")
+        menu_barras.setAttribute("aria-expanded", "false" )
+    }
+}
+
